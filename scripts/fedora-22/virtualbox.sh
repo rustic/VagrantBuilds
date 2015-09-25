679afc7ccb817f5057836c4819055f6ce6d505cf
Lee Myers <ichilegend@gmail.com>
2015-09-25 18:04:18 -0400

    Version 0.0.16

--- scripts/fedora-22/virtualbox.sh
+++ scripts/fedora-22/virtualbox.sh
@@ -8,14 +8,17 @@ if [[ `sudo facter virtual` = "vmware" ]]; then
 fi
 
 sudo dnf -y install dkms
-
+sudo dnf -y install gcc 
+sudo dnf -y install kernel-headers  
+sudo dnf -y install make
+sudo dnf -y install perl
 # Uncomment this if you want to install Guest Additions with support for X
 #sudo dnf -y install xorg-x11-server-Xorg
 
 sudo systemctl start dkms
 sudo systemctl enable dkms
 
-sudo mount -o loop,ro ~/VBoxGuestAdditions.iso /mnt/
+sudo mount -o loop,ro /home/vagrant/VBoxGuestAdditions.iso /mnt/
 sudo /mnt/VBoxLinuxAdditions.run || :
 sudo umount /mnt/
-rm -f ~/VBoxGuestAdditions.iso
+sudo rm -f /home/vagrant/VBoxGuestAdditions.iso
