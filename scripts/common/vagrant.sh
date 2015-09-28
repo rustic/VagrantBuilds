f31e720ecdefa1e6426b2303a93f55aa6ef2480c
Lee Myers <ichilegend@gmail.com>
2015-09-28 15:48:11 -0400

    Version 0.1.1 Script Update

--- scripts/common/vagrant.sh
+++ scripts/common/vagrant.sh
@@ -6,6 +6,6 @@ set -x
 date | sudo tee /etc/vagrant_box_build_time
 
 mkdir -pm 700 /home/vagrant/.ssh
-wget --no-check-certificate 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub' -O /home/vagrant/.ssh/authorized_keys
+curl -fsSLo /home/vagrant/.ssh/authorized_keys https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub
 chmod 0600 /home/vagrant/.ssh/authorized_keys
-chown -R vagrant /home/vagrant/.ssh
+chown -R vagrant:vagrant /home/vagrant/.ssh
