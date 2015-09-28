3c113ec01187753712159b5e8ac81c4eb0d169df
Lee Myers <ichilegend@gmail.com>
2015-09-28 12:44:29 -0400

    Version 0.0.17

--- scripts/fedora-22/virtualbox.sh
+++ scripts/fedora-22/virtualbox.sh
@@ -18,6 +18,8 @@ sudo dnf -y install perl
 sudo systemctl start dkms
 sudo systemctl enable dkms
 
+next_loop=$(($(ls -1 /dev/loop* | sed 's/[^0-9]//g' | LC_ALL=C sort -n | tail -1)+1)) ; mknod -m660 /dev/loop${next_loop} b 7 ${next_loop} ; chown root.disk /dev/loop${next_loop} ; unset next_loop ;
+
 sudo mount -o loop,ro /home/vagrant/VBoxGuestAdditions.iso /mnt/
 sudo /mnt/VBoxLinuxAdditions.run || :
 sudo umount /mnt/
